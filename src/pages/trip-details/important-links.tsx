2bd181c8bf5d201fae9830ef30be4e95c62bb6be
Rodrigo Mesquita <dev.rodrigomesquita@gmail.com>
2024-07-14 16:55:17 -0300

    feat: fetch trip links

--- src/pages/trip-details/important-links.tsx
+++ src/pages/trip-details/important-links.tsx
@@ -1,30 +1,41 @@
 import { Link2, Plus } from "lucide-react";
 import { Button } from "../../components/button";
+import { useEffect, useState } from "react";
+import { api } from "../../lib/axios";
+import { useParams } from "react-router-dom";
+
+interface Link {
+  id: string
+  title: string
+  url: string
+}
 
 export function ImportantLinks(){
+  const [links, setLinks] = useState<Link[]>([])
+  const { tripId } = useParams()
+
+  useEffect(() => {
+    api.get(`/trips/${tripId}/links`).then(response => setLinks(response.data.links))
+  }, [tripId])
+
+
   return (
     <div className="space-y-6">
       <h2 className="font-semibold text-xl">Links importantes</h2>
       <div className="space-y-5">
-        <div className="flex items-center justify-between gap-4">
-          <div className="space-y-1.5 flex-1">
-            <span className="block font-medium text-zinc-900">Reserva do AirBnB</span>
-            <a href='#' className="block text-xs text-zinc-600 truncate hover:text-zinc-400 transition-colors">
-              https://www.airbnb.com.br/rooms/104700011654654564645454
-            </a>
-          </div>
-          <Link2 className="text-zinc-600 size-5" />
-        </div>
-        <div className="flex items-center justify-between gap-4">
-          <div className="space-y-1.5 flex-1">
-            <span className="block font-medium text-zinc-900">Regras da casa</span>
-            <a href='#' className="block text-xs text-zinc-600 truncate hover:text-zinc-400 transition-colors">
-              https://www.notion.com/pages/1047000112354648336?adults=13&children=0&infants=0&pets=0&wishlist_item_id=11003621872995&check_in=2024-08-17&check_out=2024-08-26&source_impression_id=p3_1717600906_P3DL0E-bJZzguEci&previous_page_section_name=1000
-            </a>
-          </div>
-          <Link2 className="text-zinc-600 size-5" />
-        </div>
-        <div className=""></div>
+        {
+          links.map(link => (
+            <div key={link.id} className="flex items-center justify-between gap-4">
+              <div className="space-y-1.5 flex-1">
+                <span className="block font-medium text-zinc-900">{link.title}</span>
+                <a href={link.url} className="block text-xs text-zinc-600 truncate hover:text-zinc-400 transition-colors">
+                  {link.url}
+                </a>
+              </div>
+              <Link2 className="text-zinc-600 size-5" />
+            </div>
+          ))
+        }
       </div>
       
       <Button variant="secondary" size='full'>
