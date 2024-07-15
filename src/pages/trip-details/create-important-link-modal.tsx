45f16d7e267cbef2d5abec1ad8046277a9aab126
Rodrigo Mesquita <dev.rodrigomesquita@gmail.com>
2024-07-15 11:50:36 -0300

    feat: register new link

--- src/pages/trip-details/create-important-link-modal.tsx
+++ src/pages/trip-details/create-important-link-modal.tsx
@@ -1,11 +1,29 @@
 import { Link2, Tag, X } from "lucide-react";
 import { Button } from "../../components/button";
+import { FormEvent } from "react";
+import { api } from "../../lib/axios";
+import { useParams } from "react-router-dom";
 
 interface CreateImportantLinkModalProps {
   closeCreateLinkModal: () => void
 }
 
 export function CreateImportantLinkModal({ closeCreateLinkModal }: CreateImportantLinkModalProps){
+  const { tripId } = useParams()
+
+  async function createLink(event: FormEvent<HTMLFormElement>){
+    event.preventDefault()
+
+    const data = new FormData(event.currentTarget)
+
+    const title = data.get('title')?.toString()
+    const url = data.get('url')?.toString()
+
+    await api.post(`/trips/${tripId}/links`, { title, url })
+
+    window.document.location.reload()
+  }
+
   return (
     <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
       <div className="w-[720px] rounded-xl py-5 px-6 shadow-shape bg-zinc-100 space-y-5">
@@ -21,21 +39,21 @@ export function CreateImportantLinkModal({ closeCreateLinkModal }: CreateImporta
           </p>
         </div>
 
-        <form className='gap-3 space-y-3'>
+        <form onSubmit={createLink} className='gap-3 space-y-3'>
           <div className="flex flex-col gap-2">
             <div className="flex items-center gap-2 px-4 h-14 bg-zinc-200 border border-zinc-300 rounded-lg">
               <Tag className='size-5 text-zinc-600' />
               <input
                 type='text' 
-                name='name' 
+                name='title' 
                 className="bg-transparent text-lg placeholder-zinc-600 outline-none flex-1" placeholder="Título do link" 
               />
             </div>
             <div className="flex items-center gap-2 px-4 h-14 bg-zinc-200 border border-zinc-300 rounded-lg">
               <Link2 className='size-5 text-zinc-600' />
               <input
-                type='email' 
-                name='email' 
+                type='url' 
+                name='url' 
                 className="bg-transparent text-lg placeholder-zinc-600 outline-none flex-1" placeholder="URL" 
               />
             </div>
